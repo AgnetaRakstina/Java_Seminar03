f870e58ab83587c2c122bdba24b74c9caf2ccdd2
AgnetaRakstina <rakstinaagneta@gmail.com>
2026-03-17 11:50:15 +0200

    Edit: made constructors

--- src/model/Post.java
+++ src/model/Post.java
@@ -36,8 +36,23 @@ public class Post {
 		countOfLikes++;
 	}
 	//4. abi konstruktori
+	public Post() { //noklusejuma
+		setMsg("Sveiciens");
+		//ta ka datetime mainigiais ir inicializets uz date time -locakdatetime.now tad nav jataisa set
+		//nav jataisa set count of likes jo vienmer sak ar 0
+	}
 	
+	public Post(String inputMsg) {    //argumenta konstruktors
+		setMsg(inputMsg);
+		//nav vajazigs
+		//nav vajadzigs
+	}
 	//5. toString
+	public String toString() {
+		String result = msg + " (" + datetime + "), " + countOfLikes + " patik iezimes.";
+		return result;
+	}
+	
 	
 	//6.citas funkcijas
 	
