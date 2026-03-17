d6b5fa47e2fe976678f7315003a0551e97ff6663
AgnetaRakstina <rakstinaagneta@gmail.com>
2026-03-17 12:09:47 +0200

    Edit: made registered user class and stopped at password encryption

---
+++ src/model/users/RegiesteredUser.java
@@ -0,0 +1,59 @@
+package model.users;
+
+import java.security.MessageDigest;
+
+public class RegiesteredUser extends GuestUser { //lai viss kas bija guest useram butu ari seit
+	//1. mainigie
+	private String username;
+	private String password;
+	private String title; //privatajam lietotajam vards un uzvards, biznesa lietotajam - kompanijas nosaukums
+	
+	
+	
+	//2. getteri
+	public String getUsername() {
+		return username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public String getTitle() {
+		return title;
+	}
+	
+	//3. setteri
+	public void setUsername(String inputUsername) {
+		if(inputUsername != null &&  !inputUsername.isEmpty() && inputUsername.matches("[A-Za-z0-9]{4,20}")) {
+			username = inputUsername;
+		}
+		else {
+			username = "DefaultUser";
+		}
+	}
+	
+	public void setPassword(String inputPassword) { //TODO parbaudit != null, not empty, masku
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(password.getBytes());
+			password = md.digest().toString();
+		}
+		catch (Exception e) {
+			
+		}
+		
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	//4. abi konstrutkori
+	
+	
+	//5. toString
+	
+	
+	//6. parejas funkcijas
+	
+	
+}
